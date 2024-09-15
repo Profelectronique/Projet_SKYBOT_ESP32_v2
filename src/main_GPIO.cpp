e29cddbdd865aa1e4d86decd67c656dd4837d3b8
Prof <Profelectronique@gmail.com>
2024-09-15 18:26:37 +0200

    Code Chenillard Sens 1

--- src/main_GPIO.cpp
+++ src/main_GPIO.cpp
@@ -1,18 +1,26 @@
+///////////// Chenillard dans un sens ////////////
 #include <Arduino.h>
+int led1=1;
+int led2=2;
+int led3=3;
+int led4=4;
 
-// put function declarations here:
-int myFunction(int, int);
+int temps = 500;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+void setup()
+ {
+ pinMode(led1 , OUTPUT);
+ pinMode(led2 , OUTPUT);
+ pinMode(led3 , OUTPUT);
+ pinMode(led4 , OUTPUT);
+ }
+void loop()
+ {
+  for (int i=1; i<5;i++)
+  {
+ digitalWrite(i , HIGH);
+ delay(temps);
+ digitalWrite(i , LOW);
+  }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+ }
